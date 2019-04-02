344fce5e619446e4495badb88516f56f536d8a1b
Thierry Joux <tjoux@hotmail.com>
2019-04-02 15:49:25 -0700

    Completed Stretch goals on the clojure.js file

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -84,4 +84,6 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+  
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -5,7 +5,7 @@ function embellish() {
   let title = "simple title";
 
   function formatter() {
-    title = "*****************" + title + "********************";
+    title = "@@@@@@@@@@@@@@@@  " + title + "  @@@@@@@@@@@@@@@@@@";
     console.log(title);
   }
   return formatter;
@@ -19,16 +19,46 @@ innerFunction();
 
 
 // ==== Challenge 2: Create a counter function ====
+
+console.log("*********************  STRETCH CHALLENGE: Increment a counter **********************")
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  function increment() {
+    console.log(++count);
+  }
+  return increment;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+newCounter = counter();
+//Example usage: const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
+
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+
+console.log("*********************  STRETCH CHALLENGE: Create object with increment and decrement functions **********************")
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  let methodObject = {
+    increment: function() {console.log(++count);},
+    decrement: function() {console.log(--count);}
+  }
+  return methodObject;
 };
+
+let myObject = counterFactory();
+myObject.increment();
+myObject.increment();
+myObject.increment();
+myObject.decrement();
+myObject.increment();
+myObject.decrement();
+// myObject.increment();
+
+
